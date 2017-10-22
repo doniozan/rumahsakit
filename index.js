6e57cd6bc66db897fa8d4f2751190129caeab6d6
doniozan <32379314+doniozan@users.noreply.github.com>
2017-10-22 12:25:46 +0700

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use('/', function ( req, res, next) {
     res.header("Access-Control-Allow-Methods","GET, POST, OPTIONS, PUT, DELETE");    
     next();
 });
-let loginRoute = require('./login/loginRoute');
+let loginRoute = require('./perawat/loginRoute');
 app.use('/api',loginRoute);
 let pasienRoute = require('./pasien/pasienRoute');
 app.use('/api',verifyToken,pasienRoute);
