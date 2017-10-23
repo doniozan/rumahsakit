c87dc65369edcfa708d45e86b0d604411de2b2e1
doniozan <32379314+doniozan@users.noreply.github.com>
2017-10-23 11:07:32 +0700

    Update index.js

--- index.js
+++ index.js
@@ -21,8 +21,8 @@ let satuanHargaDokterRoute = require('./satuanHargaDokter/satuanHargaDokterRoute
 app.use('/api',satuanHargaDokterRoute);
 let perawatRoute = require('./perawat/perawatRoute');
 app.use('/api',perawatRoute);
-let userPetugasRoute = require('./userPetugas/userPetugasRoute');
-app.use('/api',userPetugasRoute);
+let rumahsakitRoute = require('./rumahsakit/rumahsakitRoute');
+app.use('/api',rumahsakitRoute);
 mong.connect('mongodb://doni:h4g4t4doniozan@ds121345.mlab.com:21345/rumahsakit');
 app.listen(process.env.PORT || 8889, function() {
   console.log('Node app is running on port', app.get('port'));
